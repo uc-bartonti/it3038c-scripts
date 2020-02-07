880c14697ce207d34d9b17debc9ffda38084d73f
uc-bartonti <bartonti@mail.uc.edu>
2020-02-07 10:27:22 -0500

    adding project script

---
+++ powershell/project1.ps1
@@ -0,0 +1,10 @@
+﻿Function Alpha {
+Add-Type -AssemblyName System.Speech
+$SpeechSynthesizer = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+$Size = Get-WmiObject -Class Win32_logicaldisk -Filter "DriveType ='3'" | ForEach-Object {[math]::Truncate($_.freespace / 1GB)}
+$SpeechSynthesizer.SelectVoice('Microsoft Zira Desktop')
+$SpeechSynthesizer.Rate = 2
+$SpeechSynthesizer.Speak("The date and time is $(Get-Date)")
+$SpeechSynthesizer.Speak("C drive free space is $Size gigabytes")
+}
+Alpha
