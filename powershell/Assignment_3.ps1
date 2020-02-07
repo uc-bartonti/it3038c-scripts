e343f41682f924d64b307ddb86253fa1711a4a92
uc-bartonti <bartonti@mail.uc.edu>
2020-02-07 09:43:40 -0500

    adding powershell files

---
+++ powershell/Assignment_3.ps1
@@ -0,0 +1,6 @@
+﻿function getIP{
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+}
+$IP = getIP
+Write-Host("This Machine's IP is $IP")
+Send-MailMessage -To "tbarton38@yahoo.com" -From "tbarton38@yahoo.com" -Subject "IT3038c windows systeminfo" -Body "body" -SmtpServer smtp.mail.yahoo.com -Port 465 -UseSsl -Credential (Get-Credential)
---
+++ powershell/sysinfo1.ps1
@@ -0,0 +1,21 @@
+﻿function getIP{
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+}
+Function Date{ 
+   (Get-Date)
+}
+Function Host2{ 
+    ($Host).Name   
+}
+Function Power{ 
+    ($Host).Version
+}
+$IP = getIP
+$date = Date
+$Host2 = Host2
+$User = User
+$power = Power
+$body = "This Machine's IP is $IP User is $env:USERNAME Powershell version $Power
+today's date is $date"
+Send-MailMessage -To "tbaronti@gmail.com" -From "t-barton@outlook.com" -Subject "IT3038c windows systeminfo" -Body $body -SmtpServer smtp.gmail.com -Port 587 -UseSsl -Credential (Get-Credential)
+#Write-Host($body)
